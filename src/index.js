fec6ea0babdb826ff2c295173595892d2767075a
Serhii Bakun <gizzmo599@gmail.com>
2022-10-24 00:01:02 +0300

    hw v.1.0
    without "infinite scrolling"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './css/styles.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import SimpleLightbox from 'simplelightbox';
 import { Notify } from 'notiflix';
+
 import markup from './js/templates/cards.hbs';
 import axiosGet from './js/axios';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
   form: document.querySelector('.search-form'),
@@ -16,6 +18,7 @@ let request = '';
 
 refs.form.addEventListener('submit', onSubmit);
 refs.laodMore.addEventListener('click', onLaodMore);
+refs.gallery.addEventListener('click', onGalleryClick);
 
 async function onSubmit(e) {
   e.preventDefault();
@@ -31,12 +34,33 @@ async function onSubmit(e) {
       Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
+      return;
     }
     renderGallery(response.data.hits);
     if (response.data.totalHits > 40) {
       refs.laodMore.hidden = false;
     }
     Notify.success(`"Hooray! We found ${response.data.totalHits} images."`);
+    lightbox();
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+
+async function onLaodMore() {
+  try {
+    page += 1;
+    const response = await axiosGet(request, page);
+    const quantity =
+      document.querySelectorAll('.photo-card').length +
+      response.data.hits.length;
+    if (response.data.totalHits <= quantity) {
+      refs.laodMore.hidden = true;
+      Notify.info("We're sorry, but you've reached the end of search results.");
+    }
+    renderGallery(response.data.hits);
+    lightbox();
+    slowScroll();
   } catch (e) {
     console.log(e.message);
   }
@@ -52,20 +76,24 @@ function resetRender() {
   refs.gallery.innerHTML = '';
 }
 
-async function onLaodMore() {
-  try {
-    page += 1;
-    const response = await axiosGet(request, page);
-    const quantity =
-      document.querySelectorAll('.photo-card').length +
-      response.data.hits.length;
-    console.log(quantity);
-    if (response.data.totalHits <= quantity) {
-      refs.laodMore.hidden = true;
-      Notify.info("We're sorry, but you've reached the end of search results.");
-    }
-    renderGallery(response.data.hits);
-  } catch (e) {
-    console.log(e.message);
-  }
+function onGalleryClick(evt) {
+  evt.preventDefault();
+}
+
+function lightbox() {
+  let lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+  });
+  lightbox.refresh();
+}
+
+function slowScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2.4,
+    behavior: 'smooth',
+  });
 }
